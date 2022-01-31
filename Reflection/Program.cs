a2b9f1d838ca476ac61fd00f883adb8eaa3e4d33
Shchoholiev <sh.codework@gmail.com>
2022-01-31 19:09:35 +0200

    Added assemblies load, created class instance and other.

---
+++ Reflection/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+var assemblyInterface = Assembly.LoadFrom("Education_dotNet_Reflection_interface");
+var interfaceType = assemblyInterface.GetTypes()
+                                  .Where(t => t.Name == "IInterface")
+                                  .FirstOrDefault();
+
+var assemblyClasses = Assembly.LoadFrom("Education_dotNet_Reflection_classes");
+var myClass = assemblyClasses.GetTypes()
+                             .Where(c => interfaceType.IsAssignableFrom(c))
+                             .FirstOrDefault();
+
+var classInstance = assemblyClasses.CreateInstance(myClass.ToString());
+classInstance?.GetType()?.GetProperty("CurrentIndex")?.SetValue(classInstance, 5);
+Console.WriteLine(classInstance?.GetType()?.GetMethod("GetNextIndex")?.Invoke(classInstance, null));
