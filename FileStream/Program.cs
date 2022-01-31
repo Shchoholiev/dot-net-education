1d4dc5958c304071057dab599ab176dd5935e577
Shchoholiev <sh.codework@gmail.com>
2022-01-31 19:48:51 +0200

    Added reading user input, adding to list, writing to file and then writing from file to console

---
+++ FileStream/Program.cs
@@ -0,0 +1,21 @@
+﻿var text = new List<string>();
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Type your text: ");
+    text.Add(Console.ReadLine());
+}
+
+Console.WriteLine();
+
+using (var sw = new StreamWriter(@"text.txt"))
+{
+    foreach (var line in text)
+    {
+        sw.WriteLine(line);
+    }
+}
+
+using (var sr = new StreamReader(@"text.txt"))
+{
+    Console.WriteLine(sr.ReadToEnd());
+}
