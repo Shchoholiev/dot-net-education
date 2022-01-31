6445fa46de059ad5ed1ad9846f9399928db39123
Shchoholiev <sh.codework@gmail.com>
2022-01-31 21:07:24 +0200

    Added project with serialization and deserialization of object

---
+++ Project/Education_dotNet_Serialization_class/ClassA.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Education_dotNet_Serialization_class
+{
+    [Serializable]
+    public class Order
+    {
+        public int Count;
+
+        public decimal Price;
+
+        [NonSerialized]
+        public decimal TotalPrice;
+
+        public Order(int count, decimal price)
+        {
+            this.Count = count;
+            this.Price = price;
+
+            this.TotalPrice = count * price;
+        }
+    }
+}
---
+++ Project/Serialization/Program.cs
@@ -0,0 +1,17 @@
+﻿using Education_dotNet_Serialization_class;
+using System.Runtime.Serialization.Formatters.Binary;
+
+var order = new Order(4, 4.2m);
+
+var bf = new BinaryFormatter();
+using (var fs = new FileStream("serialized.dat", FileMode.Create))
+{
+    bf.Serialize(fs, order);
+}
+
+using (var fs = new FileStream("serialized.dat", FileMode.Open))
+{
+    var deserialized = (Order)bf.Deserialize(fs);
+    var orderDeserialized = new Order(deserialized.Count, deserialized.Price);
+    Console.WriteLine(orderDeserialized.TotalPrice);
+}
