21f25aef3b3b4c1fe63eaef63d6b6feaebe3af61
Shchoholiev <sh.codework@gmail.com>
2022-02-21 18:50:08 +0200

    Added start of ado repository

---
+++ ORMs/ORMs/ORMs.DAL/Repository/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using ORMs.Core.Entities;
+
+namespace ORMs.DAL.Repository
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        void Add(TEntity entity);
+
+        TEntity GetOne(int id);
+
+        List<TEntity> GetAll();
+
+        void Update(TEntity entity);
+
+        void Delete(int id);
+    }
+}
---
+++ ORMs/ORMs/ORMs.DAL/StudentsADORepository.cs
@@ -0,0 +1,60 @@
+﻿using ORMs.Core.Entities;
+using ORMs.DAL.Repository;
+using System.Data.SqlClient;
+
+namespace ORMs.DAL
+{
+    public class StudentsADORepository : IGenericRepository<Student>
+    {
+        private readonly string _connectionString;
+
+        public StudentsADORepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void Add(Student student)
+        {
+            var command = new SqlCommand($"INSERT INTO dbo.[Student] " +
+              $"VALUES {student.Name}, {student.Surname}, {student.Age}, {student.Dormitory?.Id}, " +
+              $"{student.RecordBook.Id}, {student.Department.Id}");
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }                
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    connection?.Close();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Student> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student GetOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
